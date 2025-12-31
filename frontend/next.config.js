92fe5026e321e615e6c8cf49f0bb3e00248e57b6
umercplus33dev <umercplusoft@outlook.com>
2025-12-31 18:46:42 +0500

    update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,76 +4,8 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'images.unsplash.com',
-                port: '',
-                pathname: '/**',
+                hostname: '**',
             },
-            {
-                protocol: 'https',
-                hostname: 's.alicdn.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'encrypted-tbn0.gstatic.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'khanboutique.pk',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'wearzones.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'i.pinimg.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'clothsvilla.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'cdn.shopify.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'jadeblue.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'www.noorangi.pk',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: '1ststep.pk',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'www.shutterstock.com',
-                port: '',
-                pathname: '/**',
-            }
         ],
     },
 }
